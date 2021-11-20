c175ab1ce8a1bee3221b782985773cedf302bc00
unknown <bahadir0646@gmail.com>
2021-11-20 17:21:55 +0100

    I added components and api.

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import useStyles from "./styles";
+const Form = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Form;
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+import Post from "./Post/Post";
+import useStyles from "./styles";
+
+const Posts = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <h1>Posts</h1>
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
